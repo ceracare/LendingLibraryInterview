7fda308f2323438e05060b08630065516bba2922
Samuel Pereira <pereira.samuel@ceracare.co.uk>
2024-10-09 14:09:46 +0100

    improvements to EF setup and add instructions to readme

---
+++ LendingLibraryInterview.Tests/tests/Services/InMemoryDatabaseTestsBase.cs
@@ -0,0 +1,17 @@
+﻿using LendingLibraryInterview.Data;
+using Microsoft.EntityFrameworkCore;
+
+namespace LendingLibraryInterview.Tests.tests.Services;
+public abstract class InMemoryDatabaseTestsBase
+{
+    protected InMemoryDatabaseTestsBase()
+    {
+        var options = new DbContextOptionsBuilder<SQLiteDbContext>()
+                .UseInMemoryDatabase(databaseName: "SQLiteDbContext")
+                .Options;
+
+        DbContext = new SQLiteDbContext(options);
+    }
+
+    protected SQLiteDbContext DbContext { get; }
+}
